336a031e44d3d83306f710adac06700c6f74f824
cnowak7 <rafi.slapass@gmail.com>
2016-05-21 20:18:50 -0500

    fixed sign up redirect bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to login_path, notice: "Thank you for signing up!"
+			redirect_to home_catalog_path, notice: "Thank you for signing up!"
 		else
 			render 'new'
 		end
